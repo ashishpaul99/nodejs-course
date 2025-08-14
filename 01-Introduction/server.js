697a0a265b417bae695d0f35387109b69ebd90fc
ashishpaul99 <ashishpaul1199@gmail.com>
2025-08-14 12:26:08 +0530

    Update notes

--- 01-Introduction/server.js
+++ 01-Introduction/server.js
@@ -2,29 +2,45 @@
 // console.log(global);
 
 // Common core modules
-const os = require('os');
-const path = require('path');
+// const os = require('os');
+// const path = require('path');
+// // const math=require("./math");
 
-// OS information
-console.log("OS Type:", os.type()); 
-console.log("OS Version:", os.version());
+// // // // OS information
+// console.log("OS Type:", os.type()); 
+// console.log("OS Version:", os.version());
 
-// Home directory
-console.log("Home Directory:", os.homedir()); 
+// // Home directory
+// console.log("Home Directory:", os.homedir()); 
 
-// Other values we can access in Node.js
-// Current directory name
-console.log("Directory Name:", __dirname);
+// // Other values we can access in Node.js
+// // Current directory name
+// console.log("Directory Name:", __dirname);
 
-// Current file name
-console.log("File Name:", __filename);
-console.log("Directory Name:", path.dirname(__filename));
-console.log("Base File Name:", path.basename(__filename));
-console.log("Extension Name:", path.extname(__filename));
+// // Current file name
+// console.log("File Name:", __filename);
+// console.log("Directory Name:", path.dirname(__filename));
+// console.log("Base File Name:", path.basename(__filename));
+// console.log("Extension Name:", path.extname(__filename));
 
-// Returns an object with detailed information about the file path
-console.log(path.parse(__filename));
-console.log(path.parse(__filename).name);
+// // Returns an object with detailed information about the file path
+// console.log(path.parse(__filename));
+// console.log(path.parse(__filename).name);
+
+
+// math functions
+// const math=require("./math");
+// console.log(math.add(10,20)); //o/p:30
+// console.log(math.sub(20,10)); //o/p:10
+// console.log(math.mul(10,20)); //o/p:200
+// console.log(math.div(20,10)); //o/p:2
+
+// alternative :destructuring
+const {add,sub,mul,div}=require("./math");
+console.log(add(10,20)); //o/p:30
+console.log(sub(20,10)); //o/p:10
+console.log(mul(10,20)); //o/p:200
+console.log(div(20,10)); //o/p:2
 
 // Output:
 // $ node server
@@ -48,3 +64,7 @@ console.log(path.parse(__filename).name);
 // We can also install and use packages created by other developers from npm.
 // NPM stands for Node Package Manager.
 // We can also create our own custom modules.
+
+
+
+
